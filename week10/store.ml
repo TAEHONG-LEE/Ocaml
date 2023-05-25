b394ca4e84ee46fa9c5c7dc8331e163b6dd75575
hong <hong@DESKTOP-IF3MBGQ>
2023-05-25 22:08:23 +0900

    Commit week10

---
+++ week10/store.ml
@@ -0,0 +1,15 @@
+type t = (string * value) list
+and value = NumV of int | ClosureV of string * Ast.expr * t
+
+let empty : t = []
+
+let rec find (str : string) (lst : t) =
+  match lst with
+  | [] -> failwith ("[Error] Free identifier: "^str)
+  | (s, v) :: t -> if s = str then v else find str t
+
+let rec add (str : string) (v : value) (lst : t) =
+  match lst with
+  | [] -> (str, v) :: lst
+  | (s, e) :: t -> if s = str then (str, v) :: t else (s, e) :: add str v t
+
